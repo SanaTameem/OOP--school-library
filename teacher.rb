2a823f9cc385b393968b82fb98631a81d5fc8891
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-09-13 19:34:20 +0430

    Add  teacher.rb file

---
+++ teacher.rb
@@ -0,0 +1,21 @@
+require_relative 'person'
+
+class Teacher < Person
+  attr_reader :specialization
+
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
+# teacher = Teacher.new(35,"Physics","John")
+# puts teacher.name
+# puts teacher.id
+# puts teacher.age
+# puts teacher.can_use_services?
+# puts teacher.specialization
