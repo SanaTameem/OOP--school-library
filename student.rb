6b32035a164e5242d50f9b022c4f53ccdf3f3f77
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-09-15 20:54:04 +0430

    Update student.rb file

--- student.rb
+++ student.rb
@@ -12,7 +12,7 @@ class Student < Person
     '¯\(ツ)/¯'
   end
 
-  def add_classroom(classroom)
+  def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
