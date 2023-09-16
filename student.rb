1b646349ba8eb0f6edd8b1250503849a6bd398a4
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-09-16 17:41:38 +0430

    Update student.rb file

--- student.rb
+++ student.rb
@@ -1,19 +1,11 @@
 require_relative 'person'
 
 class Student < Person
-  attr_reader :classroom
-
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
+  def initialize(age, _parent_permission, name = 'Unknown')
+    super(age, name)
   end
 
   def play_hooky
     '¯\(ツ)/¯'
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
