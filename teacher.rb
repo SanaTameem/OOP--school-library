90b6ca32046179bdccbc6f9ee2c23d12f0b4fd9c
SanaTameem <71990110+SanaTameem@users.noreply.github.com>
2023-09-21 18:49:48 +0430

    Update teacher.rb file

--- teacher.rb
+++ teacher.rb
@@ -3,12 +3,16 @@ require_relative 'person'
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(age, specialization, name = 'Unknown')
-    super(age, name)
+  def initialize(id, age, specialization, name = 'Unknown')
+    super(id, age, name)
     @specialization = specialization
   end
 
   def can_use_services?
     true
+  end
+
+  def to_json(options = {})
+    super(options)
   end
 end
